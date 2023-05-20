a75a421b561e407c43e547e3798799e2a7e77020
Jackie <4087635+gersteinj@users.noreply.github.com>
2023-05-19 22:40:37 -0400

    minor cleanup

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,20 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
+###############################################
+#                                             #
+#              Utility Functions              #
+#                                             #
+###############################################
+
 def convert_name(display_name: str):
     return display_name.replace(' ','-').lower()
+
+###############################################
+#                                             #
+#            Get Individual Robot             #
+#                                             #
+###############################################
 
 def get_robot(db: Session, robot_id: int):
     return db.query(models.Robot).filter(models.Robot.id == robot_id).first()
@@ -15,11 +27,23 @@ def get_robot_by_display_name(db: Session, display_name: str):
 def get_robot_by_name(db: Session, name: str):
     return db.query(models.Robot).filter(models.Robot.name == name).first()
 
+###############################################
+#                                             #
+#             Get List of Robots              #
+#                                             #
+###############################################
+
 def get_robots(db: Session, skip: int=0, limit: int=100):
     return db.query(models.Robot).offset(skip).limit(limit).all()
 
 def get_robots_by_weight_class(db: Session, weight_class: str):
     return db.query(models.Robot).filter(models.Robot.weight_class == weight_class).all()
+
+###############################################
+#                                             #
+#                Create Robot                 #
+#                                             #
+###############################################
 
 def create_robot(db: Session, robot: schemas.Robot):
     db_robot = models.Robot(display_name=robot.display_name, weight_class=robot.weight_class, name=convert_name(robot.display_name))
