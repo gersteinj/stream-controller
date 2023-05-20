dddddeb2b766ab33b9e1ac60cc33de07aa108803
Jackie <4087635+gersteinj@users.noreply.github.com>
2023-05-19 20:12:53 -0400

    can add robots to database

---
+++ app/crud.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def convert_name(display_name: str):
+    return display_name.replace(' ','-').lower()
+
+def get_robot(db: Session, robot_id: int):
+    return db.query(models.Robot).filter(models.Robot.id == robot_id).first()
+
+def get_robot_by_display_name(db: Session, display_name: str):
+    converted_name = convert_name(display_name)
+    print(f'Checking to see if {converted_name} already exists')
+    return db.query(models.Robot).filter(models.Robot.name == converted_name).first()
+
+def get_robot_by_name(db: Session, name: str):
+    return db.query(models.Robot).filter(models.Robot.name == name).first()
+
+def get_robots(db: Session, skip: int=0, limit: int=100):
+    return db.query(models.Robot).offset(skip).limit(limit).all()
+
+def create_robot(db: Session, robot: schemas.Robot):
+    db_robot = models.Robot(display_name=robot.display_name, weight_class=robot.weight_class, name=convert_name(robot.display_name))
+    db.add(db_robot)
+    db.commit()
+    db.refresh(db_robot)
+    return db_robot
---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close()
+
+@app.post('/robots/', response_model=schemas.Robot)
+def create_robot(robot: schemas.Robot, db: Session = Depends(get_db)):
+    db_robot = crud.get_robot_by_display_name(db, display_name=robot.display_name)
+    if db_robot:
+        raise HTTPException(status_code=400, detail="Name already used!")
+    return crud.create_robot(db=db, robot=robot)
+
+@app.get('/robots/', response_model=list[schemas.Robot])
+def read_robots(skip: int=0, limit: int=50, db: Session = Depends(get_db)):
+    robots = crud.get_robots(db, skip=skip, limit=limit)
+    return robots
+
+@app.get('/robots/{robot_id}', response_model=schemas.Robot)
+def read_robot(robot_id: int, db: Session = Depends(get_db)):
+    db_robot = crud.get_robot(db, robot_id=robot_id)
+    if db_robot is None:
+        raise HTTPException(status_code=404, detail="Robot Not Found :(")
+    return db_robot
