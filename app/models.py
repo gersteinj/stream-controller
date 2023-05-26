7273713b5c96b286d44eaf9901a64da2826c6feb
Jackie <4087635+gersteinj@users.noreply.github.com>
2023-05-26 11:42:17 -0400

    enums work

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship
 
 from .database import Base
+from .myEnums import WeightsEnum
 
 
 class Robot(Base):
@@ -10,5 +11,5 @@ class Robot(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
     display_name = Column(String)
-    weight_class = Column(String)
+    weight_class = Column(Enum(WeightsEnum))
     
---
+++ app/myEnums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+class WeightsEnum(Enum):
+    ANT = 'ANT'
+    PLANT = 'PLANT'
+    BEETLE = 'BEETLE'
