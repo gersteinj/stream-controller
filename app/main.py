f36c7f73185306611cdff057a003a8fe3241c894
Jackie <4087635+gersteinj@users.noreply.github.com>
2023-05-28 18:34:39 -0400

    cleanup

--- app/main.py
+++ app/main.py
@@ -44,13 +44,6 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
-@app.post('/robots/', response_model=schemas.Robot)
-def create_robot(robot: schemas.Robot, db: Session = Depends(get_db)):
-    db_robot = crud.get_robot_by_display_name(db, display_name=robot.display_name)
-    if db_robot:
-        raise HTTPException(status_code=400, detail="Name already used!")
-    return crud.create_robot(db=db, robot=robot)
-
 @app.get('/robots/', response_model=list[schemas.Robot])
 def read_robots(skip: int=0, limit: int=50, db: Session = Depends(get_db)):
     robots = crud.get_robots(db, skip=skip, limit=limit)
@@ -61,11 +54,18 @@ def read_robots_by_weight(weight: Weights, db: Session = Depends(get_db)):
     robots = crud.get_robots_by_weight(db, weight=weight)
     return robots
 
-@app.get('/robots/{robot_id}', response_model=schemas.Robot)
+@app.post('/robots/', response_model=schemas.Robot)
+def create_robot(robot: schemas.Robot, db: Session = Depends(get_db)):
+    db_robot = crud.get_robot_by_display_name(db, display_name=robot.display_name)
+    if db_robot:
+        raise HTTPException(status_code=400, detail="Name already used!")
+    return crud.create_robot(db=db, robot=robot)
+
+@app.get('/robots/id/{robot_id}', response_model=schemas.Robot)
 def read_robot(robot_id: int, db: Session = Depends(get_db)):
     db_robot = crud.get_robot(db, robot_id=robot_id)
     if db_robot is None:
-        raise HTTPException(status_code=404, detail="Robot Not Found :(")
+        raise HTTPException(status_code=404, detail="Robot Not Found :( ")
     return db_robot
 
 @app.get('/weight/')
